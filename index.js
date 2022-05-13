98d4b5ad2a4dacd961a68920b126943a53749854
Ankit Biswal <i_ankit.b@olcademy.in>
2022-05-13 17:28:19 +0530

    db url changed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const session = require("express-session");
 const MongoDBStore = require('connect-mongo')(session);
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/moviesapp';
 
-mongoose.connect('mongodb://localhost:27017/moviesapp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("DB connected!!");
     })
